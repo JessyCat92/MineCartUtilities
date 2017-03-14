8c2be90a9ec0658ae1da0f7e918b3d3029a80f6d
Colin Weber <colin.weber@sap.com>
2017-03-14 15:46:26 +0100

    template

---
+++ src/main/java/me/geramy92/minecartutilities/MineCartUtilities.java
@@ -0,0 +1,10 @@
+package me.geramy92.minecartutilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Geramy92 on 14.03.2017.
+ */
+public class MineCartUtilities extends JavaPlugin{
+
+}
