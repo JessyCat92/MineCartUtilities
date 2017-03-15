49a9152adb7ba33b88c3d634973ad3943bdbc4d5
Colin Weber <colin.weber@sap.com>
2017-03-15 13:55:15 +0100

    try to add config

--- src/main/java/me/geramy92/minecartutilities/MineCartUtilities.java
+++ src/main/java/me/geramy92/minecartutilities/MineCartUtilities.java
@@ -1,7 +1,12 @@
 package me.geramy92.minecartutilities;
 
 import me.geramy92.minecartutilities.utilities.Logger;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MineCartUtilities extends JavaPlugin {
     private static MineCartUtilities mineCartUtilitiesInstance;
@@ -10,6 +15,26 @@ public class MineCartUtilities extends JavaPlugin {
     public void onEnable() {
         MineCartUtilities.mineCartUtilitiesInstance = this;
 
+        // ensure config file
+        File configFile = new File(MineCartUtilities.getInstance().getDataFolder() + File.separator + "config.yml");
+        if (!configFile.exists()) {
+            MineCartUtilities.getInstance().saveResource("config.yml", true);
+        }
+
+        try {
+            getConfig().load(configFile);
+        } catch (InvalidConfigurationException e) {
+            Logger.error(e.getMessage());
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            Logger.error(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+        Logger.debug(getConfig().getString("keepChunksLoaded"));
         Logger.success("Started MineCartUtilities successfully");
     }
 
