55a1a569d966d245cf6f45d9489fd97a3a2eb64c
Colin Weber <colin.weber@sap.com>
2017-03-15 12:58:56 +0100

    refactor a bit

--- src/main/java/me/geramy92/minecartutilities/utilities/Logger.java
+++ src/main/java/me/geramy92/minecartutilities/utilities/Logger.java
@@ -2,54 +2,44 @@ package me.geramy92.minecartutilities.utilities;
 
 import me.geramy92.minecartutilities.MineCartUtilities;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class Logger {
-    public class AnsiColors {
-        public static final String BLACK = "\u001B[30m";
-        public static final String RED = "\u001B[31m";
-        public static final String GREEN = "\u001B[32m";
-        public static final String YELLOW = "\u001B[33m";
-        public static final String BLUE = "\u001B[34m";
-        public static final String MAGENTA = "\u001B[35m";
-        public static final String CYAN = "\u001B[36m";
-        public static final String WHITE = "\u001B[37m";
-
-        private AnsiColors(){}
-    }
+    private Logger() {}
 
     public static void info(String msg) {
-        Logger.runLogging("info", AnsiColors.CYAN, msg);
+        Logger.runLogging("info", ChatColor.BLUE, msg);
     }
 
     public static void error(String msg) {
-        Logger.runLogging("error", AnsiColors.RED, msg);
+        Logger.runLogging("error", ChatColor.RED, msg);
     }
 
     public static void debug(String msg) {
-        Logger.runLogging("debug", AnsiColors.BLUE, msg);
+        Logger.runLogging("debug", ChatColor.DARK_BLUE, msg);
     }
 
     public static void success(String msg) {
-        Logger.runLogging("info", AnsiColors.GREEN, msg);
+        Logger.runLogging("info", ChatColor.GREEN, msg);
     }
 
-    public static void customInfo(String color, String msg) {
+    public static void customInfo(ChatColor color, String msg) {
         Logger.runLogging("info", color, msg);
     }
 
     public static void warning(String msg) {
-        Logger.runLogging("warning", AnsiColors.YELLOW, msg);
+        Logger.runLogging("warning", ChatColor.YELLOW, msg);
     }
 
     public static void log(String msg) {
-        Logger.runLogging("log", AnsiColors.WHITE, msg);
+        Logger.runLogging("log", ChatColor.WHITE, msg);
     }
 
-    private static void runLogging(String logLevel, String color, String msg) {
+    private static void runLogging(String logLevel, ChatColor color, String msg) {
         String name = MineCartUtilities.getInstance().getDescription().getName();
         String version = MineCartUtilities.getInstance().getDescription().getVersion();
 
-        String sendMsg = "[" + name + " " + version + "][" + logLevel.toUpperCase() + "] " + msg;
+        String sendMsg = "[" + name + " " + version + "] [" + logLevel.toUpperCase() + "] " + msg;
         Bukkit.getConsoleSender().sendMessage(color + sendMsg);
     }
 }
